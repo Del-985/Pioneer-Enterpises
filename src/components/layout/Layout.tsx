87f636f7a6aea55570ec3c474bc1bbbb915546b7
Del-985 <119140366+Del-985@users.noreply.github.com>
2026-07-13 12:57:55 -0400

    Create Layout.tsx

---
+++ src/components/layout/Layout.tsx
@@ -0,0 +1,20 @@
+import { Outlet } from "react-router-dom";
+
+import Header from "./Header";
+import Footer from "./Footer";
+
+function Layout() {
+  return (
+    <>
+      <Header />
+
+      <main className="main-content">
+        <Outlet />
+      </main>
+
+      <Footer />
+    </>
+  );
+}
+
+export default Layout;
