acff15139c194854264dbac54574cac575c98ef1
Del-985 <119140366+Del-985@users.noreply.github.com>
2026-07-13 12:44:40 -0400

    Create main.tsx

---
+++ src/main.tsx
@@ -0,0 +1,19 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+
+import App from "./App";
+import "./styles/global.css";
+
+const rootElement = document.getElementById("root");
+
+if (!rootElement) {
+  throw new Error(
+    'Unable to start Pioneer Management Group: the root element with id="root" was not found.'
+  );
+}
+
+createRoot(rootElement).render(
+  <StrictMode>
+    <App />
+  </StrictMode>
+);
