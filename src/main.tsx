0348d1fc82e70ae6f2d042356eed06d3c4678b7f
Del-985 <119140366+Del-985@users.noreply.github.com>
2026-07-14 13:58:05 -0400

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from "react-dom/client";
 
 import App from "./App";
 import "./styles/global.css";
+import "./styles/admin.css";
 
 const rootElement = document.getElementById("root");
 
