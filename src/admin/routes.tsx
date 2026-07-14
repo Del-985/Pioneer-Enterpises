8c6b049f18888267991b36eb0b2dc8777b727c0c
Del-985 <119140366+Del-985@users.noreply.github.com>
2026-07-14 10:27:09 -0400

    Create routes.tsx

---
+++ src/admin/routes.tsx
@@ -0,0 +1,21 @@
+import type { RouteObject } from "react-router-dom";
+
+import AdminLayout from "./layout/AdminLayout";
+import Overview from "./pages/Overview";
+
+export const adminRoutes: RouteObject[] = [
+  {
+    path: "/admin",
+    element: <AdminLayout />,
+    children: [
+      {
+        index: true,
+        element: <Overview />
+      },
+      {
+        path: "overview",
+        element: <Overview />
+      }
+    ]
+  }
+];
