78c4414ae8ecae570d164b33df1ca161bbded1a6
Del-985 <119140366+Del-985@users.noreply.github.com>
2026-07-15 19:24:16 -0400

    use landscaping app public entry point

--- src/router.tsx
+++ src/router.tsx
@@ -1,7 +1,7 @@
 import { createBrowserRouter } from "react-router-dom";
 
 import { adminRoutes } from "./admin/routes";
-import { landscapingRoutes } from "./divisions/landscaping/routes";
+import { landscapingRoutes } from "./divisions/landscaping";
 import { websiteRoutes } from "./website/routes";
 
 export const router = createBrowserRouter([
