140650c22b1b235cf4d50db5fb792aa90cfb92a2
Del-985 <119140366+Del-985@users.noreply.github.com>
2026-07-15 11:07:08 -0400

    feat(debug): render temporary development navigation

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,15 @@
 import { RouterProvider } from "react-router-dom";
 
+import DebugToolbar from "./shared/components/DebugToolbar";
 import { router } from "./router";
 
 function App() {
-  return <RouterProvider router={router} />;
+  return (
+    <>
+      <RouterProvider router={router} />
+      {import.meta.env.DEV ? <DebugToolbar /> : null}
+    </>
+  );
 }
 
 export default App;
