2979555b021bd095a7e455e67b6b339eb4d19d9b
Del-985 <119140366+Del-985@users.noreply.github.com>
2026-07-14 22:06:46 -0400

    feat(admin): add calendar route

--- src/admin/routes.tsx
+++ src/admin/routes.tsx
@@ -1,6 +1,7 @@
 import type { RouteObject } from "react-router-dom";
 
 import AdminLayout from "./layout/AdminLayout";
+import Calendar from "./pages/Calendar";
 import Overview from "./pages/Overview";
 
 export const adminRoutes: RouteObject[] = [
@@ -15,6 +16,10 @@ export const adminRoutes: RouteObject[] = [
       {
         path: "overview",
         element: <Overview />
+      },
+      {
+        path: "calendar",
+        element: <Calendar />
       }
     ]
   }
