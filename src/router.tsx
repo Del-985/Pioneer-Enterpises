d67d476dc8591d1acbe737060eb24439e77b1a56
Del-985 <119140366+Del-985@users.noreply.github.com>
2026-07-15 19:23:56 -0400

    mount landscaping as independent app

--- src/router.tsx
+++ src/router.tsx
@@ -1,9 +1,11 @@
 import { createBrowserRouter } from "react-router-dom";
 
 import { adminRoutes } from "./admin/routes";
+import { landscapingRoutes } from "./divisions/landscaping/routes";
 import { websiteRoutes } from "./website/routes";
 
 export const router = createBrowserRouter([
   ...websiteRoutes,
+  ...landscapingRoutes,
   ...adminRoutes
 ]);
