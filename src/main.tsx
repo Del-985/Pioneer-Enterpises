17035b3a9d94666361373d5f24de9c5a09a80c50
Del-985 <119140366+Del-985@users.noreply.github.com>
2026-07-15 22:09:58 -0400

    Load landscaping customer page styles

--- src/main.tsx
+++ src/main.tsx
@@ -6,6 +6,7 @@ import "./styles/global.css";
 import "./styles/home.css";
 import "./styles/landscaping.css";
 import "./styles/landscaping-pages.css";
+import "./styles/landscaping-customer-pages.css";
 import "./styles/admin.css";
 import "./styles/dashboard-customizer.css";
 import "./styles/dashboard-form-settings.css";
